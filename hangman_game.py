ea80c7f7a834d0e6c0c61f2c600bfbc78358d58d
Abhay <abh.mrya@gmail.com>
2024-07-31 11:45:56 +0530

    push

---
+++ hangman_game.py
@@ -0,0 +1,11 @@
+import random
+#word_list
+print("START GAME")
+words=input("write word\n")
+#words_list=words.split( )
+print(words)
+total_words=0
+for i in range(0,words+1):
+    #total_words=words.count()
+    total_words=total_words+1
+print(total_words)
